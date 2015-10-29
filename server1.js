b7d04534e7cf958dc925cb2ce6605a62a36a1eb2
Chandni Soni <chandnisoni.mail@gmail.com>
2015-10-29 11:28:38 -0700

    code for math on values of photosensor

--- server1.js
+++ server1.js
@@ -8,7 +8,7 @@ board.on("ready", function() {
   // Create a new `potentiometer` hardware instance.
   potentiometer = new five.Sensor({
     pin: "A0",
-    freq: 250
+    freq: 500
   });
 
   // Inject the `sensor` hardware into
@@ -17,9 +17,30 @@ board.on("ready", function() {
   board.repl.inject({
     pot: potentiometer
   });
-
+  var array = [];
+  var initAvg = 0;
   // "data" get the current reading from the potentiometer
   potentiometer.on("data", function() {
+
     console.log(this.value);
+    array.push(this.value);
+    if(array.length > 5){
+      array.shift();
+    }else{
+      initAvg = this.value;
+    }
+
+    var sum = 0;
+    for(var i=0; i<array.length; i++){
+      sum+= array[i];
+    }
+    var avg = sum/array.length;
+    console.log('average'+ avg);
+    if(avg < initAvg -20){
+      console.log('SCored!!');
+    }
+
   });
+
+
 });
